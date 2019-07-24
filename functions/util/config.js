2dda1a541ee2e1ee6538aca8146c853dea2a2fd1
JDVapor <46424734+JDVapor@users.noreply.github.com>
2019-07-23 19:59:05 -0700

    Delete config.js

--- functions/util/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apiKey: "AIzaSyDmnSFx39uwdYsEftKCfGUxJuZIce9vEaU",
-  authDomain: "socialmediaapp-847eb.firebaseapp.com",
-  databaseURL: "https://socialmediaapp-847eb.firebaseio.com",
-  projectId: "socialmediaapp-847eb",
-  storageBucket: "socialmediaapp-847eb.appspot.com",
-  messagingSenderId: "373092526181",
-  appId: "1:373092526181:web:392d7c661c32ddb2"
-};
